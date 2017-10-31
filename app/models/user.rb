9e80549c954242e98e9247a651aa52dcf1805b21
nynhex <nynhex@gmail.com>
2017-10-31 08:06:36 -0500

    Wires up association, adds validation, custom method to auth via user/email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,36 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ #Associations
+ has_many :links
+
+ attr_accessor :login
+ # Validations
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+  validate :validate_username
+  validates :username, :email, presence: true
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
